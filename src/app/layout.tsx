bb7f792d69cfca72af7c18a7d41b6db8d2154594
Math-ieu <atsoumathieu228@gmail.com>
2026-03-25 11:36:19 +0100

    feat: Implement lazy loading for tracking scripts by extracting them into a new component.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,7 @@ import dynamic from "next/dynamic";
 
 const WhatsAppSidebar = dynamic(() => import("@/components/WhatsAppSidebar"));
 const ScrollToTop = dynamic(() => import("@/components/ScrollToTop"));
+import TrackingScripts from "@/components/TrackingScripts";
 
 const workSans = Work_Sans({
     subsets: ["latin"],
@@ -71,44 +72,7 @@ export default function RootLayout({
                 <link rel="manifest" href="/site.webmanifest" />
             </head>
             <body className="antialiased min-h-screen flex flex-col overflow-x-hidden" suppressHydrationWarning>
-                <noscript>
-                    <iframe
-                        src="https://www.googletagmanager.com/ns.html?id=GTM-MCPC5PJJ"
-                        height="0"
-                        width="0"
-                        style={{ display: "none", visibility: "hidden" }}
-                    />
-                </noscript>
-                <Script
-                    id="gtm-script"
-                    strategy="lazyOnload"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','GTM-MCPC5PJJ');
-                        `,
-                    }}
-                />
-                <Script
-                    id="ga-script"
-                    strategy="lazyOnload"
-                    src={`https://www.googletagmanager.com/gtag/js?id=AW-17907112455`}
-                />
-                <Script
-                    id="ga-config"
-                    strategy="lazyOnload"
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-                            gtag('config', 'AW-17907112455');
-                        `,
-                    }}
-                />
+                <TrackingScripts />
                 <QueryProvider>
                     <TooltipProvider>
                         <ScrollToTop />
--- src/components/ServiceHeroSection.tsx
+++ src/components/ServiceHeroSection.tsx
@@ -300,25 +300,6 @@ const ServiceHeroSection = ({ title, description, image, primaryColor, isCollaps
                     </div>
                 </div>
             </div>
-            {faqs && faqs.length > 0 && (
-                <script
-                    type="application/ld+json"
-                    dangerouslySetInnerHTML={{
-                        __html: JSON.stringify({
-                            "@context": "https://schema.org",
-                            "@type": "FAQPage",
-                            "mainEntity": faqs.map((faq) => ({
-                                "@type": "Question",
-                                "name": faq.question,
-                                "acceptedAnswer": {
-                                    "@type": "Answer",
-                                    "text": faq.answer
-                                }
-                            }))
-                        })
-                    }}
-                />
-            )}
         </section>
     );
 };
