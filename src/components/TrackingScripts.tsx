ae922e66eaa16b3f847aae132a15e19c4ea953ae
Math-ieu <atsoumathieu228@gmail.com>
2026-05-12 09:19:46 +0100

    refactor: remove deferred script loading and restrict GTM execution to production domains while updating type references

--- src/components/TrackingScripts.tsx
+++ src/components/TrackingScripts.tsx
@@ -1,40 +1,9 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Script from "next/script";
 
 export default function TrackingScripts() {
-    const [loadScripts, setLoadScripts] = useState(false);
-
-    useEffect(() => {
-        let timeout: NodeJS.Timeout;
-        const handleInteraction = () => {
-            if (!loadScripts) {
-                setLoadScripts(true);
-            }
-        };
-
-        // Fallback: charge les scripts après 4 secondes si aucune interaction
-        timeout = setTimeout(() => handleInteraction(), 4000);
-
-        window.addEventListener("scroll", handleInteraction, { once: true, passive: true });
-        window.addEventListener("mousemove", handleInteraction, { once: true, passive: true });
-        window.addEventListener("touchstart", handleInteraction, { once: true, passive: true });
-        window.addEventListener("keydown", handleInteraction, { once: true, passive: true });
-        window.addEventListener("click", handleInteraction, { once: true, passive: true });
-
-        return () => {
-            clearTimeout(timeout);
-            window.removeEventListener("scroll", handleInteraction);
-            window.removeEventListener("mousemove", handleInteraction);
-            window.removeEventListener("touchstart", handleInteraction);
-            window.removeEventListener("keydown", handleInteraction);
-            window.removeEventListener("click", handleInteraction);
-        };
-    }, [loadScripts]);
-
-    if (!loadScripts) return null;
-
     return (
         <>
             <noscript>
@@ -50,11 +19,15 @@ export default function TrackingScripts() {
                 strategy="afterInteractive"
                 dangerouslySetInnerHTML={{
                     __html: `
-                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                        })(window,document,'script','dataLayer','GTM-MCPC5PJJ');
+                        if (window.location.hostname === 'agencemenage.ma' || window.location.hostname === 'www.agencemenage.ma') {
+                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                            })(window,document,'script','dataLayer','GTM-MCPC5PJJ');
+                        } else {
+                            console.log('GTM loading skipped: not on production domain');
+                        }
                     `,
                 }}
             />
